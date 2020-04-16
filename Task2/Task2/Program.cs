540aaf33a929c06c8a6a57e1005ade7503fea839
RedaKud <reda.kudreviciute@gmail.com>
2020-04-16 17:13:28 +0300

    Folders

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+            int sum = firstNum + secondNum;
+
+            Console.WriteLine("Results: {0}", sum);
+        }
+    }
+}
